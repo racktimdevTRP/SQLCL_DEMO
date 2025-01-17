446f034cc6b1bb9055dd2da78717b7b37ce21333
racktimdevTRP <racktim.guin@techriskpartners.com>
2025-01-17 11:24:32 +0000

    New comment

---
+++ src/database/sys/object_grants/object_grants_as_grantor.sys.mle language.javascript.sql
@@ -0,0 +1,6 @@
+grant execute on sys.javascript to irm_demo;
+
+
+
+
+-- sqlcl_snapshot {"hash":"38859da74e7380a2d8021e9acb31e250bb037951","type":"OBJECT_GRANT","name":"object_grants_as_grantor.SYS.MLE LANGUAGE.JAVASCRIPT","schemaName":"ADMIN"}
